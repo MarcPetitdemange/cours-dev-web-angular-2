bcf8e6c83178a596e6cb0c83fcfb77f19c379af3
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-29 23:49:29 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ColorPickerModule} from 'primeng/colorpicker';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AccordionModule} from 'primeng/accordion';
+import {ButtonModule} from "primeng/button";
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ColorPickerModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    ButtonModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
